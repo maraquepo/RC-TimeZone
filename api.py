86040f7222930b792e7dd2e49e9516fff68c6120
maraquepo <maraquepo@gmail.com>
2024-01-21 15:59:48 -0600

    all api calls

--- api.py
+++ api.py
@@ -1,17 +1,45 @@
 import requests
+import time
 from config import config
 
-def get_time_zone():
+def get_api_key():
   config_data = config()
 
   if 'apikey' not in config_data:
     raise Exception('API key is missing in the configuration.')
 
-  api_key = config_data['apikey']
-  url = f'http://api.timezonedb.com/v2.1/list-time-zone?key={api_key}&format=json'
-  response = requests.get(url)
+  return config_data['apikey']
+
+def get_time_zone():
+  api_key = get_api_key()
+
+  parameters = {
+    'key': api_key,
+    'format': 'json'
+  }
+
+  url = 'http://api.timezonedb.com/v2.1/list-time-zone'
+  response = requests.get(url, params=parameters)
+
   if response.status_code == 200:
     return response.json()
   else:
-    raise Exception("There was an error retrieving the data. Status code: " + str(response.status_code))
+    raise Exception("There was an error retrieving the data. ",  response.text)
 
+def get_details(zones):
+  api_key = get_api_key()
+
+  parameters = {
+    'key': api_key,
+    'format': 'json',
+    'by': 'zone',
+    'zone': zones
+  }
+
+  url = 'http://api.timezonedb.com/v2.1/get-time-zone'
+  response = requests.get(url, params=parameters)
+  time.sleep(3)
+  if response.status_code == 200:
+    return response.json()
+  else:
+    raise Exception("There was an error retrieving the data. ",  response.text)
