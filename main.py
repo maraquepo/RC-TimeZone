757bbc6bb7ef2c65676022f8cd509f03c805cc66
maraquepo <maraquepo@gmail.com>
2024-01-21 14:38:04 -0600

    create connection to db

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def test():
-  print("Testing...")
+import sqlite3
 
 def main():
-  test()
+  try:
+    connection = sqlite3.connect('/timezone.db')
+    cursor = connection.cursor()
+  except Exception as e:
+    print("There was an error creating the database")
+    print(e)
+
+  connection.commit()
 
 if __name__ == "__main__":
   main()
