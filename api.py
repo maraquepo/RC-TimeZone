3fea19fd3f4389b1fa6913b24098a0e6c8dd010c
maraquepo <maraquepo@gmail.com>
2024-01-21 15:37:46 -0600

    api call to get time zones

--- api.py
+++ api.py
@@ -1,3 +1,17 @@
 import requests
+from config import config
 
-def get_time_zone()
+def get_time_zone():
+  config_data = config()
+
+  if 'apikey' not in config_data:
+    raise Exception('API key is missing in the configuration.')
+
+  api_key = config_data['apikey']
+  url = f'http://api.timezonedb.com/v2.1/list-time-zone?key={api_key}&format=json'
+  response = requests.get(url)
+  if response.status_code == 200:
+    return response.json()
+  else:
+    raise Exception("There was an error retrieving the data. Status code: " + str(response.status_code))
+
